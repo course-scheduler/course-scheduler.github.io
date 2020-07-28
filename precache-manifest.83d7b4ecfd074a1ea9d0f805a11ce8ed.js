d4ae029515377e3c058443a132e9898ab0acc4a1
course-scheduler <xlabcourse.scheduler@gmail.com>
2020-07-28 16:09:46 -0600

    Added react app

---
+++ precache-manifest.83d7b4ecfd074a1ea9d0f805a11ce8ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d27b5f20f062663c0f3fd64f806b335",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8028a69643ffda218cb",
+    "url": "/static/css/main.93b36e40.chunk.css"
+  },
+  {
+    "revision": "770b077ee313a5e63446",
+    "url": "/static/js/2.5a7793a4.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/static/js/2.5a7793a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8028a69643ffda218cb",
+    "url": "/static/js/main.e46c7349.chunk.js"
+  },
+  {
+    "revision": "95f80e643efd576bfbb7",
+    "url": "/static/js/runtime-main.3af7b923.js"
+  }
+]);
